5a49cd5c90a58bca778d73ebad9ed9d183418408
Sean Slicer <seanslicer@gmail.com>
2022-09-13 02:40:22 -0500

    init cmmt

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { FC } from "react";
+import { Image, StyleSheet, View, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: FC = () => (
+  <View>
+    <Text>SoSoc</Text>
+  </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
