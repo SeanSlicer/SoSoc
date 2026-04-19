89fb9ffcc0519f99f2c1edeff7618992e12482f4
Sean Slicer <722sas17@gmail.com>
2026-04-19 18:41:47 -0500

    suppress hydration warning on html element caused by theme script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={geist.variable}>
+    <html lang="en" className={geist.variable} suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
